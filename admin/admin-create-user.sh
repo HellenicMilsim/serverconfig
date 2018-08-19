eca0234e804cb4e05ac8b6256e26bec2c6078367
Tony K <aklin@users.noreply.github.com>
2018-08-19 17:40:38 +0300

    Add more scripts and docs
    Add job: server-restart
    Add task: create-user
    Add usergroup documentation

---
+++ admin/admin-create-user.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+# Create user and set their public key
+TS="date +%Y-%m-%d-%H:%M:%S"
+user=$1
+key=$2
+userdir=/home/$user
+groups=(a3-mission-uploaders a3-log-viewers)
+
+echo "${TS} - Starting task: create-user"
+
+useradd -Dm $user
+
+echo "${TS} - Adding key"
+# Create required directories
+mkdir $userdir/.ssh
+touch $userdir/.ssh/authorized_keys
+
+chown -R $user:$user $userdir/.ssh
+
+# Copy file over
+echo "$key" >> $userdir/.ssh/authorized_keys
+
+# Set permissions for openssl to work
+chmod 700 $userdir/.ssh
+chmod 600 $userdir/.ssh/authorized_keys
+
+
+# Add user to default groups
+for group in Groups
+do
+	echo "${TS} - Adding user, group: $user -> $group"
+	useradd -G $group $user
+done
+
+echo "${TS} - End task: create-user"
---
+++ doc/user-roles-description.md
@@ -0,0 +1,21 @@
+# Server Setup
+
+## User Groups
+
+* sudoers
+* a3-mission-uploaders
+* a3-log-viewers
+
+
+### a3-mission-uploaders
+Read-write access to mpmissions folder
+
+### a3-log-viewers
+Read access to logs folder
+
+
+## Scheduled tasks
+
+
+
+A3Server-related tasks are run via crontab from the same user as the A3Server.
