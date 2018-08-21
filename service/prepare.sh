6aefd5a0816169f7152461460dd08c4db9ebdb76
Tony K <aklin@users.noreply.github.com>
2018-08-21 20:21:53 +0300

    fix statement

--- service/prepare.sh
+++ service/prepare.sh
@@ -5,6 +5,7 @@
 function create_secret {
 	name=$1
 	if grep --quiet $name docker secret ls; then
+		echo "Secret exists: $name"
 	else
 		openssl rand -base64 32 | docker secret create $name -
 	fi
@@ -14,7 +15,6 @@ function create_secret {
 #create_secret "db_name"
 create_secret "db_user"
 create_secret "db_password"
-create_secret "db_root_password"
 
 
 #TODO docker volumes
